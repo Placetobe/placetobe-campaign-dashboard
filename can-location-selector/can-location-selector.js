cdaae9bf500c5a4a42a0844e8a880532a07c4ca8
Wouter Vroege <wouter@woutervroege.nl>
2017-12-15 13:38:13 +0100

    style location window

--- can-location-selector/can-location-selector.js
+++ can-location-selector/can-location-selector.js
@@ -2,40 +2,81 @@ Polymer({
     is: 'can-location-selector',
 
     properties: {
+
+        query: {
+            type: String,
+            observer: '_queryChanged'
+        },
+        
         geocode: {
             type: Object,
-            reflectToAttribute: true,
             notify: true,
             value: {
                 lat: 52.0910391,
                 lng: 5.132362199999989
             }
         },
-        formattedAddress: {
-            type: String
-        },
+
         results: Array,
+
+        options: {
+            type: Array,
+            computed: '_computeOptions(results.splices)'
+        },
+
+
+        noResults: {
+            type: Array,
+            computed: '_computeNoResults(results.splices)'
+        },
+
+        chosenAddress: {
+            type: String,
+            observer: '_focusOnMarker'
+        },
         
         searchMapsString: String
-    },
-    
-    listeners: {
-        'google-map-search-results': 'resizeModal'
+
     },
 
-    _focusOnMarker: function(evt, data) {
-        console.log(evt.model.marker);
+
+    _queryChanged: function() {
+        this.set('results', []);
+        this.set('geocode.lat', null);
+        this.set('geocode.lng', null);
+        this.set('chosenAddress', null);
+        this.set('geocode', {});
+    },
+
+    _computeNoResults: function() {
+        return !this.results || this.results.length < 1;
+    },
+    
+    _computeOptions: function(resultsSplice) {
+        var results = this.results;
+        return results.map(function(result) {
+            return {
+                label: result.name,
+                value: [result.latitude, result.longitude]
+            }
+        })
+    },
+
+    _focusOnMarker: function(chosenAddress) {
+        if(!chosenAddress) return;
+        var geo = chosenAddress.split(',');
         this.set('geocode', {
-            lat: evt.model.marker.latitude,
-            lng: evt.model.marker.longitude,
+            lat: geo[0],
+            lng: geo[1],
         });
         this.$$("google-map").fitToMarkers = true;
-        this.set('formattedAddress', evt.model.marker.formatted_address)
     },
+
     _searchGoogleMaps: function() {
         this.$$("#searchselector").set('query', this.$$('#searchMapsString').value);
         this.$$("#searchselector").search();
     },
+
     resizeModal: function() {
         this.fire('resizeModal');
     }
